4fc8d9bbf83e3d80c47420a8402dbc45b5fc4d4a
Sayan650 <sayan650@gmail.com>
2023-12-03 09:42:32 +0530

    frontend page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-white'>Home</div>
+    <div className='text-white'>
+        <h1>Welcome to the home page!</h1>
+        <img className='w-708 h-auto' src="hero.jpg" alt="hero" />
+    </div>
   )
 }
 
